52af1d7b3a0c1dc0a3096ea5dfbb49feebc967c0
Matthew Rende <69356465+mrende1986@users.noreply.github.com>
2023-05-23 16:41:49 -0700

    Add files via upload

---
+++ dags/hockey_etl_pipeline.py
@@ -0,0 +1,32 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime
+from yearly import run_hockey_ref_to_s3
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 11, 8),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'hockey_etl_pipeline_dag',
+    default_args=default_args,
+    description='Pipeline to extract game logs from hockey-reference.com',
+    schedule_interval=timedelta(days=1),
+)
+
+run_etl = PythonOperator(
+    task_id='complete_run_hockey_ref_to_s3',
+    python_callable=run_hockey_ref_to_s3,
+    dag=dag, 
+)
+
+run_etl
---
+++ dags/yearly.py
@@ -0,0 +1,39 @@
+import requests
+import boto3
+import configparser
+import pandas as pd
+
+def run_hockey_ref_to_s3():
+    result = pd.DataFrame()
+    for i in range (2020,2024):
+        year = str(i)
+        print(year)
+        source = requests.get('https://www.hockey-reference.com/players/h/hugheja03/gamelog/'+year+'#gamelog').text
+        
+        df = pd.read_html(source,header=1)[0]
+        df['year'] = year
+        result = result.append(df, sort=False)
+
+    result.columns = ["Rk","Date","Game","Age","Tm","HA","Opp","WL","Goal","Assist","PTS","plus_minus","PIM","EVen","PPlay","SHand","GW","EV","PP","SH","S","S_pct","SHFT","TOI","HIT","BLK","FOW","FOL","FO_pct","year"]
+    result = result[result.Rk != 'Rk']
+
+    # save dataframe to local drive
+    local_filename = 'jackhuges-v3.csv'
+    result.to_csv(local_filename, index=False)
+    print("CSV Saved")
+
+    s3 = boto3.client(
+            's3',
+            aws_access_key_id="", aws_secret_access_key="")
+    print("S3 connection setup")
+    s3_file = local_filename
+    bucket_name = ""
+
+
+    s3.upload_file(
+        local_filename,
+        bucket_name,
+        s3_file)
+    print("File uploaded")
+
+run_hockey_ref_to_s3()
